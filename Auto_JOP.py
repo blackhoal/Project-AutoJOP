93c2b8c4b08e2b48c73cfbf747c16c8853123591
blackhoal <48504392+blackhoal@users.noreply.github.com>
2021-03-21 16:02:05 +0900

    Create Auto_JOP.py

---
+++ Auto_JOP.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from selenium import webdriver
+from haversine import haversine
+from folium.features import DivIcon
+from email.mime.text import MIMEText
+import requests
+import json
+import time
+import googlemaps
+import folium
+import polyline
+import os
+import boto3
+import smtplib
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
