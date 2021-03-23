c1501d2719641cd643031eb20fe57e025515c739
blackhoal <48504392+blackhoal@users.noreply.github.com>
2021-03-23 22:06:05 +0900

    Create ChatBot.py

---
+++ ChatBot.py
@@ -0,0 +1,18 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+class TelegramBot:
+    def __init__(self, name, token):
+        self.core = telegram.Bot(token)
+        self.updater = Updater(token)
+        self.id = "ID 입력"
+        self.name = name
+
+    def sendMessage(self, text):
+        self.core.sendMessage(chat_id = self.id, text=text)
+
+    def stop(self):
+        self.updater.start_polling()
+        self.updater.dispatcher.stop()
+        self.updater.job_queue.stop()
+        self.updater.stop()
